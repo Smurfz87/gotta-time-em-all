4626b3da2466a5d1c8291919a744399f09025e55
Stian Mikal Nygaard <stian.mikal.nygaard@posten.no>
2026-06-16 09:00:49 +0200

    feat: config for gh pages

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
+const repo = process.env.GITHUB_REPOSITORY?.split('/')[1]
+
 export default defineConfig({
+  base: repo ? `/${repo}/` : '/',
   plugins: [svelte()]
 })
